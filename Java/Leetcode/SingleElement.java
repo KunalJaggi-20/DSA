a4f9637af4f9edfd5fbe81e28c1b0a893da3631b
Kunal Jaggi <kunaljaggi@Kunals-MacBook-Air.local>
2026-09-23 02:21:40 +0530

    Single Element Changes

--- Java/Leetcode/SingleElement.java
+++ Java/Leetcode/SingleElement.java
@@ -7,7 +7,7 @@ public class SingleElement
         int arr[] = {1, 1, 2, 3, 3, 4, 4, 8, 8};
         int ans = arr[0];
 
-        for(int i=1 ; i<arr.length ; i++)
+        for(int i=1 ; i<arr.length-1 ; i++)
         {
             if(arr[i] != arr[i-1] && arr[i] != arr[i+1])
             {
