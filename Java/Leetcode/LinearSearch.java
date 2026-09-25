1959b899dffda536756cd8663d9a6d78daa6488f
Kunal Jaggi <kunaljaggi@Kunals-MacBook-Air.local>
2026-09-26 01:03:43 +0530

    Max Consecutive 1s & Linear Search

---
+++ Java/Leetcode/LinearSearch.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class LinearSearch 
+{
+    public static void main(String[] args) 
+    {
+        int arr[] = {1, 2, 3, 4, 5};
+        int target = 4;
+       
+        for(int i=0 ; i<arr.length ; i++)
+        {
+            if(arr[i] == target)
+            {
+                System.out.println("Element found at index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }    
+}
